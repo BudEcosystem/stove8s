1392e29c2f0be8aba37a7119ea97c0bd0476cc9d
sinanmohd <sinan@sinanmohd.com>
2025-09-17 10:00:14 +0530

    feat(controller/snapshot): init

--- cmd/main.go
+++ cmd/main.go
@@ -36,6 +36,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	stovek8sv1beta1 "bud.studio/stovek8s/api/v1beta1"
+	"bud.studio/stovek8s/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -47,6 +50,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(stovek8sv1beta1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -198,6 +202,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := (&controller.SnapShotReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SnapShot")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
