9dceebd0b907a4550fb804ba5c3091624b7c0783
sinanmohd <sinan@sinanmohd.com>
2025-09-26 16:16:46 +0530

    chore(operator/reconciler): init

--- internal/controller/snapshot_controller.go
+++ internal/controller/snapshot_controller.go
@@ -18,8 +18,20 @@ package controller
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"slices"
 
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	apitypes "k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -27,10 +39,17 @@ import (
 	stove8sv1beta1 "bud.studio/stove8s/api/v1beta1"
 )
 
+const podCaCertPath = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+
 // SnapShotReconciler reconciles a SnapShot object
 type SnapShotReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme        *runtime.Scheme
+	kubeletClient http.Client
+}
+
+type CheckPointResp struct {
+	Items []string `json:"items"`
 }
 
 // +kubebuilder:rbac:groups=stove8s.bud.studio,resources=snapshots,verbs=get;list;watch;create;update;patch;delete
@@ -39,23 +58,172 @@ type SnapShotReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the SnapShot object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
+
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
 func (r *SnapShotReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	log := logf.FromContext(ctx)
 
-	// TODO(user): your logic here
+	snapshot := &stove8sv1beta1.SnapShot{}
+	err := r.Get(ctx, req.NamespacedName, snapshot)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// If the custom resource is not found then it usually means that it was deleted or not created
+			// In this way, we will stop the reconciliation
+			log.Info("memcached resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get memcached")
+		return ctrl.Result{}, err
+	}
+
+	pod, requeue, err := r.podFromObjectRef(ctx, snapshot.Spec.Selector.Object)
+	if err != nil {
+		if requeue {
+			return ctrl.Result{}, nil
+		} else {
+			return ctrl.Result{}, err
+		}
+	}
+
+	snapshot.Status.Stage = stove8sv1beta1.CriuDumping
+	// checkPointNodePath, err := r.checkpoint(ctx, pod, snapshot.Spec.Selector.Container)
+	_, err = r.checkpoint(ctx, pod, snapshot.Spec.Selector.Container)
+	if err != nil {
+		if requeue {
+			return ctrl.Result{}, nil
+		} else {
+			return ctrl.Result{}, err
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
 
+func (r *SnapShotReconciler) checkpoint(ctx context.Context, pod *corev1.Pod, containerName string) (string, error) {
+	log := logf.FromContext(ctx)
+
+	idx := slices.IndexFunc(pod.Spec.Containers, func(container corev1.Container) bool {
+		if container.Name == containerName {
+			return true
+		}
+		return false
+	})
+	if idx < 0 {
+		err := errors.New("No such container")
+		log.Error(err, containerName)
+		return "", err
+	}
+
+	node := corev1.Node{}
+	err := r.Get(ctx, apitypes.NamespacedName{Name: pod.Spec.NodeName}, &node)
+	if err != nil {
+		log.Error(err, "Failed to get node")
+		return "", err
+	}
+
+	idx = slices.IndexFunc(node.Status.Addresses, func(addr corev1.NodeAddress) bool {
+		if addr.Type == corev1.NodeInternalIP {
+			return true
+		}
+
+		return false
+	})
+	if idx < 0 {
+		err := errors.New("No internaIP for node")
+		log.Error(err, node.Name)
+		return "", err
+	}
+
+	url := fmt.Sprintf(
+		"https://%v:%v/checkpoint/%v/%v/%v",
+		node.Status.Addresses[idx].Address,
+		node.Status.DaemonEndpoints.KubeletEndpoint,
+		pod.ObjectMeta.Namespace,
+		pod.Name,
+		containerName,
+	)
+	checkpointReq, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		log.Error(err, "Creating http request object")
+		return "", err
+	}
+	resp, err := r.kubeletClient.Do(checkpointReq)
+	if err != nil {
+		log.Error(err, "checkpoint request failed")
+		return "", err
+	}
+
+	var cr CheckPointResp
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Error(err, "Reading response body")
+		return "", err
+	}
+
+	err = json.Unmarshal(body, &cr)
+	if err != nil {
+		log.Error(err, "Unmarsheling response")
+		return "", err
+	}
+
+	if len(cr.Items) != 1 {
+		err := errors.New("Unexpected output, ooga booga")
+		log.Error(err, node.Name)
+		return "", err
+	}
+
+	return cr.Items[0], nil
+}
+
+func (r *SnapShotReconciler) podFromObjectRef(ctx context.Context, obj corev1.ObjectReference) (*corev1.Pod, bool, error) {
+	log := logf.FromContext(ctx)
+	pod := &corev1.Pod{}
+	var namespacedName apitypes.NamespacedName
+
+	switch obj.Kind {
+	// TODO: implement deployment, job, etc...
+	case "Pod":
+		namespacedName = apitypes.NamespacedName{
+			Namespace: obj.Namespace,
+			Name:      obj.Name,
+		}
+	default:
+		err := errors.New("Unsupported Kind")
+		log.Error(err, obj.Kind)
+		return nil, false, err
+	}
+
+	err := r.Get(ctx, namespacedName, pod)
+	if err != nil {
+		log.Error(err, "Failed to get pod")
+		return nil, true, err
+	}
+
+	return pod, false, nil
+}
+
 // SetupWithManager sets up the controller with the Manager.
 func (r *SnapShotReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	caCert, err := os.ReadFile(podCaCertPath)
+	if err != nil {
+		return err
+	}
+	caCertPool := x509.NewCertPool()
+	ok := caCertPool.AppendCertsFromPEM(caCert)
+	if !ok {
+		return errors.New("Failed to get cert from PEM")
+	}
+
+	r.kubeletClient = http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: caCertPool,
+			},
+		},
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&stove8sv1beta1.SnapShot{}).
 		Named("snapshot").
