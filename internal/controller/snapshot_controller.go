fc3fe3b27e93854fb0a48e3caeaf792791c216d4
sinanmohd <sinan@sinanmohd.com>
2025-11-11 16:19:16 +0530

    chore(daemonset/service): init

--- internal/controller/snapshot_controller.go
+++ internal/controller/snapshot_controller.go
@@ -44,7 +44,7 @@ import (
 
 const (
 	podCaCertPath     = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-	daemonsetNodePort = 80085
+	daemonsetNodePort = 38008
 )
 
 // SnapShotReconciler reconciles a SnapShot object
