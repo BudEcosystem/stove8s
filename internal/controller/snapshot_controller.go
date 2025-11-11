227aa0b363a847007c49e47dc32b58da979053e1
sinanmohd <sinan@sinanmohd.com>
2025-11-11 16:49:54 +0530

    fix(helm): fixes from testing phase

--- internal/controller/snapshot_controller.go
+++ internal/controller/snapshot_controller.go
@@ -61,6 +61,7 @@ type CheckPointResp struct {
 // +kubebuilder:rbac:groups=stove8s.bud.studio,resources=snapshots,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=stove8s.bud.studio,resources=snapshots/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=stove8s.bud.studio,resources=snapshots/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;update;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
